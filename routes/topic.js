634ce8679033dcfb8394c98b947c3a1a50938af5
LIM JANGBIN <great_jb2005@naver.com>
2025-11-22 11:26:35 +0900

    Create topic.js

---
+++ routes/topic.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+var qs = require('querystring');
+
+
+router.get("/create", (request, response) => 
+        var title = 'WEB - create';
+        var list = template.list(request.list);
+        var html = template.HTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `, '');
+        response.send(html);
+);
+
+router.post("/create_process", (request, response) =>  // form 에서 post 방식으로 전송했기 때문 
+     var post = request.body;
+     var title = post.title;
+     var description = post.description;
+     fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+       response.writeHead(302, {Location: `/?id=${title}`});
+       response.end();
+       response.redirect(`/page/${title}`);
+);
+
+router.get("/update/:pageId", (request, response) =>
+        var filteredId = path.parse(request.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+            var title = request.params.pageId;
+            var list = template.list(request.list);
+            var html = template.HTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+            );
+            response.send(html);
+        });
+);
+
+router.post("/update_process", (request, response) =>  
+     var request.body;
+     var id = post.id;
+     var title = post.title;
+     var description = post.description;
+     fs.rename(`data/${id}`, `data/${title}`, function(error){
+       fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+         response.redirect(`/page/${title}`);
+       });
+     });
+);
+
+router.post("/delete_process", (request, response) =>
+     var post = request.body;
+     var id = post.id;
+     var filteredId = path.parse(id).base;
+     fs.unlink(`data/${filteredId}`, function(error){
+       response.redirect('/');
+     });
+
+router.get("/page/:pageId", (request, response, next) => // 미들웨어 순서에 의해 cru가 먼저 라우트 되어야함
+     var filteredId = path.parse(request.params.pageId).base;
+     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+       if(err) {
+            next(err); // 아무것도 없거나 'route'가 아닌 경우는 에러를 던지는 것으로 내부적으로 약속
+       } else {
+            var title = request.params.pageId;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {allowedTags:['h1']});
+            var list = template.list(request.list);
+            var html = template.HTML(sanitizedTitle, list,
+              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+              `<a href="/create">create</a>
+               <a href="/update/${sanitizedTitle}">update</a> // routing params 이용하므로 ?id=${} 제거
+               <form action="/delete_process" method="post"> // /delete... 라고 안적으면 눌렀을 때, /page의 하위에서 delete로 이동!
+               <input type="hidden" name="id" value="${sanitizedTitle}">
+               <input type="submit" value="delete">
+           </form>`
+       );
+       response.send(html);
+     }
+  });
+);
+);

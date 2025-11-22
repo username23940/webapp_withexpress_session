8a8dba3e07ee9219553fdd6d704111e4ea37556c
LIM JANGBIN <great_jb2005@naver.com>
2025-11-23 08:49:34 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var template = require('../lib/template.js');
- 
+
+function authIsOnwer(request, response) {
+    if(request.session.is_logined) {
+     return true
+   } else {
+     return false
+   }
+}
+
+
 router.get('/', function(request, response) { 
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-      <h2>${title}</h2>${description}
-      <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
-      `,
-      `<a href="/topic/create">create</a>`
-    ); 
-    response.send(html);
-  });
+
+   var title = 'Welcome';
+   var description = 'Hello, Node.js';
+   var list = template.list(request.list);
+   var html = template.HTML(title, list,
+     `
+     <h2>${title}</h2>${description}
+     <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
+     `,
+     `<a href="/topic/create">create</a>`
+   ); 
+   response.send(html);
+ });
    
   module.exports = router;
